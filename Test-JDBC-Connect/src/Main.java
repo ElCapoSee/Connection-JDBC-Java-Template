56a8b1f0e539b5e5682b010502434bb0b1a91a2b
ElCapoSee <elcaposee@gmail.com>
2021-01-07 16:50:46 +0600

    Update Main.java
    Add oracle jdbc connect

--- Test-JDBC-Connect/src/Main.java
+++ Test-JDBC-Connect/src/Main.java
@@ -84,3 +84,130 @@ public class Main {
         connection.close();
     }
 }
+////////////////////////////////////////////////////////////
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import oracle.jdbc.OracleTypes;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) throws SQLException {
+        String id = "gfjg";
+        long content = 39568228819L;
+        System.out.println("-------- PostgreSQL JDBC Connection Testing ------------");
+
+        try {
+            //Class.forName("org.postgresql.Driver");
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException var12) {
+            System.out.println("Where is your Oracle JDBC Driver? Include in your library path!");
+            var12.printStackTrace();
+            return;
+        }
+
+        System.out.println("Oracle JDBC Driver Registered!");
+        Connection connection = null;
+
+        /*
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://esb-dbp1-lt3.hq.bc:5432/monitoring", "esb-jdbc-test", "lY4Qt3aJ89hSD9C");
+        } catch (SQLException var11) {
+            System.out.println("Connection Failed! Check output console");
+            var11.printStackTrace();
+            return;
+        }*/
+        try {
+            connection = DriverManager.getConnection("jdbc:oracle:thin:@esb_test.hq.bc:1521:esbtest", "ESBLOGS", "Xq2!fmcTVD");
+        } catch (SQLException var11) {
+            System.out.println("Connection Failed! Check output console");
+            var11.printStackTrace();
+            return;
+        }
+
+        if (connection != null) {
+            System.out.println("You made it, take control your database now!");
+        } else {
+            System.out.println("Failed to make connection!");
+        }
+
+        /*
+        char dm = (char) 34;
+        connection.setAutoCommit(false);
+        String sql = "{call public.\"testGetProcessNames\"(?)}";
+        CallableStatement cs = connection.prepareCall(sql);
+        cs.registerOutParameter(1, 2012);
+        cs.executeUpdate();
+        ResultSet result = (ResultSet)cs.getObject(1);
+        ArrayList contents = new ArrayList(100);
+
+        while(result.next()) {
+            contents.add(result.getString(1));
+        }
+
+        Iterator var9 = contents.iterator();
+
+        while(var9.hasNext()) {
+            String data = (String)var9.next();
+            System.out.println(data);
+        }
+
+        connection.close();
+        */
+        /*
+        try {
+            final CallableStatement statement = connection.prepareCall("{call lib_logviewer.GetProcessVariableList(?,?)}");
+            try {
+                statement.setLong(1, content);
+                statement.registerOutParameter(2, OracleTypes.CURSOR);
+                statement.execute();
+                final ResultSet result = (ResultSet) statement.getObject(2);
+                int columns = result.getMetaData().getColumnCount();
+                try {
+                    final List<String> names = new ArrayList<String>(100);
+                    while (result.next()) {
+                        names.add(result.getString(1));
+                        for (int i = 1; i <= columns; i++){
+                            System.out.print(result.getString(i) + "\t");
+                        }
+                    }
+
+                    for(String data : names) {
+                        System.out.println(data);
+                    }
+                } finally {
+                    result.close();
+                }
+            } finally {
+                statement.close();
+            }
+            */
+            try {
+                final CallableStatement statement = connection.prepareCall("{call lib_secure.CalcHash(?,?)}");
+                try {
+                    statement.setString(1, id);
+                    statement.registerOutParameter(2, OracleTypes.RAW);
+                    statement.execute();
+                    String test = statement.getString(2);
+                    System.out.println(test);
+                } finally {
+                    statement.close();
+                }
+        } finally {
+            connection.close();
+        }
+    }
+
+}
